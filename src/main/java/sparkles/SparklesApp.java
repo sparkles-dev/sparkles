e50a3b4ad5193eee7fd0f0f48ca108e8741134c8
David <david@spektrakel.de>
2018-03-29 23:03:15 +0200

    feat: initialize hello world app

---
+++ src/main/java/sparkles/SparklesApp.java
@@ -0,0 +1,13 @@
+package sparkles;
+
+import static spark.Spark.*;
+
+public class SparklesApp {
+
+  public static void main(String[] args) {
+    get("/", (req, res) -> {
+        return "hello from sparkjava.com";
+    });
+  }
+
+}
