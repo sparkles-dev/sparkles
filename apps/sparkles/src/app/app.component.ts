e67d06d4ca30b10a00c221d9658346ad81518b4d
dherges <dherges@users.noreply.github.com>
2018-11-14 18:19:14 +0100

    feat: add an angular app with cypress and jest

---
+++ apps/sparkles/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sparkles-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'sparkles';
+}
