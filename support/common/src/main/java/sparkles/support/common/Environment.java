530986535fdbb05eaf4ca937d32cae4c1cc40b3e
David <david@spektrakel.de>
2018-11-17 16:48:52 +0100

    feat: extract Environment util to common package

---
+++ support/common/src/main/java/sparkles/support/common/Environment.java
@@ -0,0 +1,36 @@
+package sparkles.support.common;
+
+public enum Environment {
+  PRODUCTION,
+  DEVELOP;
+
+  public static String value(String name, String defaultValue) {
+    final String systemValue = System.getProperty(name, System.getenv(name));
+
+    return systemValue != null ? systemValue : defaultValue;
+  }
+
+  public static Environment environment() {
+    final String env = value("ENVIRONMENT", "production");
+    if ("production".equalsIgnoreCase(env)) {
+      return PRODUCTION;
+    } else if ("develop".equalsIgnoreCase(env)) {
+      return DEVELOP;
+    }
+
+    return PRODUCTION;
+  }
+
+  public static boolean isDevelop() {
+    return environment() == DEVELOP;
+  }
+
+  public static boolean isProduction() {
+    return environment() == PRODUCTION;
+  }
+
+  public static String logLevel() {
+    return environment() == DEVELOP ? "debug" : "info";
+  }
+
+}
