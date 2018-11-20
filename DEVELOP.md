1178da73660db03d633cd1e0bd185f036030573b
dherges <dherges@users.noreply.github.com>
2018-11-20 13:02:37 +0100

    style: move things around

---
+++ DEVELOP.md
@@ -0,0 +1,10 @@
+Getting Started
+===============
+
+> The needless documentation.
+
+ - `apps`: Application source code.
+ - `libs`: Library source code, shared across several apps.
+ - `support`: The supporting cast. Support libraries for non-domain tasks.
+ - `deployables`: Infrastructure as code. Collection of docker compose service(s) to run the apps.
+ - `tools`: Developer tools.
--- scripts/build-app.sh
+++ scripts/build-app.sh
@@ -21,12 +21,12 @@ echo "Building $APP with gradle..."
 ./gradlew apps:$APP:assemble
 
 echo "Copying distributions to Docker build context..."
-rm -rf tools/docker/sparkling-container/dist
-mkdir -p tools/docker/sparkling-container/dist
-cp apps/$APP/build/distributions/*.zip tools/docker/sparkling-container/dist
+rm -rf tools/docker/gradle-app/dist
+mkdir -p tools/docker/gradle-app/dist
+cp apps/$APP/build/distributions/*.zip tools/docker/gradle-app/dist
 
 echo "Building the docker container..."
-cd tools/docker/sparkling-container
+cd tools/docker/gradle-app
 docker build \
   --build-arg bin=$APP \
   --build-arg zip=$(basename *.zip .zip) \
