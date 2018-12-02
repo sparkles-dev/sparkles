c9a95830896c7d3c91c9ea5e2c96a61811038a5e
David <david@spektrakel.de>
2018-12-02 12:07:06 +0100

    feat(support/moshi): extract moshi utilities

---
+++ support/moshi/src/main/java/sparkles/support/moshi/LocalDateTimeAdapter.java
@@ -0,0 +1,34 @@
+package sparkles.support.moshi;
+
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.JsonReader;
+import com.squareup.moshi.JsonWriter;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+public class LocalDateTimeAdapter extends JsonAdapter<LocalDateTime> {
+  public static final Class<?> TYPE = LocalDateTime.class;
+
+  @Override
+  public LocalDateTime fromJson(JsonReader jsonReader) throws IOException {
+
+    JsonReader.Token token = jsonReader.peek();
+    if (token == JsonReader.Token.STRING) {
+      return LocalDateTime.parse(jsonReader.nextString());
+    } else {
+      jsonReader.skipValue();
+      return null;
+    }
+  }
+
+  @Override
+  public void toJson(JsonWriter jsonWriter, LocalDateTime localDateTime) throws IOException {
+    if (localDateTime != null) {
+      jsonWriter.value(localDateTime.toString());
+    } else {
+      jsonWriter.nullValue();
+    }
+  }
+
+}
