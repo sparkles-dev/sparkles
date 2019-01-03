1f06e05aaae47b51d866c71a386a95b3eb9948f6
David <david@spektrakel.de>
2019-01-03 21:05:04 +0100

    build: compile styles with scss, add circle config

--- packages/styles/gulpfile.babel.js
+++ packages/styles/gulpfile.babel.js
@@ -4,12 +4,17 @@ import gulp from 'gulp';
 import postcss from 'gulp-postcss';
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
-import postcssImport from 'postcss-import';
+import sass from 'gulp-sass';
+import nodeSass from 'node-sass';
+
+sass.compiler = nodeSass;
 
 function css() {
-  return gulp.src('src/styles.css')
+  return gulp.src('src/styles.scss')
+    .pipe(
+      sass().on('error', sass.logError),
+    )
     .pipe(postcss([
-      postcssImport,
       tailwindcss('tailwind.js'),
       autoprefixer
     ]))
