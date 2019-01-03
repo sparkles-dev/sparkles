4607a55ce15d6a95e4cf67ac08b76737b41f8752
David <david@spektrakel.de>
2019-01-03 20:53:31 +0100

    feat(styles): add .btn component

--- packages/styles/gulpfile.babel.js
+++ packages/styles/gulpfile.babel.js
@@ -4,10 +4,12 @@ import gulp from 'gulp';
 import postcss from 'gulp-postcss';
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
+import postcssImport from 'postcss-import';
 
 function css() {
   return gulp.src('src/styles.css')
     .pipe(postcss([
+      postcssImport,
       tailwindcss('tailwind.js'),
       autoprefixer
     ]))
