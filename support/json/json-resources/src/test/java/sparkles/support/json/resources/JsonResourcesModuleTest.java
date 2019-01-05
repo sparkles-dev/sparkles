282fedc5790ac924ca4b2b8955702a7ef5ea6edc
David <david@spektrakel.de>
2019-01-05 13:10:49 +0100

    test: json resource serialization tests

--- support/json/json-resources/src/test/java/sparkles/support/json/resources/JsonResourcesModuleTest.java
+++ support/json/json-resources/src/test/java/sparkles/support/json/resources/JsonResourcesModuleTest.java
@@ -1,37 +1,66 @@
 package sparkles.support.json.resources;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.junit.Test;
+
+import java.util.UUID;
+
+import sparkles.support.common.ToStringBuilder;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class JsonResourcesModuleTest {
 
   private final ObjectMapper om = new ObjectMapper()
-    .registerModule(new JsonResourcesModule());
+    .registerModule(new JsonResourcesModule())
+    .setSerializationInclusion(JsonInclude.Include.NON_DEFAULT);
 
   @Test
-  public void foo() throws Exception {
+  public void embedded_shouldSerialize() throws Exception {
     Foo foo = new Foo();
     foo.related = new Bar("hello world!");
 
     String serialized = om.writeValueAsString(foo);
-    //assertThat(serialized).isNull();
+    assertThat(serialized).isEqualTo("{\"_embedded\":{\"related\":{\"name\":\"hello world!\"}}}");
 
-    Foo deserialized = om.readValue(serialized, Foo.class);
-    assertThat(deserialized).isNull();
+  }
+
+  @Test
+  public void embedded_shouldDeserialize() throws Exception {
+    Foo deserialized = om.readValue("{\"_embedded\":{\"related\":{\"name\":\"hello world!\"}}}", Foo.class);
+    assertThat(deserialized).isNotNull();
+    assertThat(deserialized.related).isNotNull();
+    assertThat(deserialized.related.name).isEqualTo("hello world!");
+  }
+
+  @Test
+  public void shouldSerializeProperties() throws Exception {
+    Foo foo = new Foo();
+    foo.id = UUID.fromString("f6eb1bd7-7611-49b1-ba15-5d1fa59a30a3");
+
+    String serialized = om.writeValueAsString(foo);
+    assertThat(serialized).isEqualTo("{\"id\":\"f6eb1bd7-7611-49b1-ba15-5d1fa59a30a3\"}");
   }
 
   @Resource
   private static class Foo {
 
+    @Links
+    public LinkCollection links;
+
     @Embedded
     public Bar related;
 
+    public UUID id;
+
     @Override
     public String toString() {
-      return "Foo[related=" + related.toString() + "]";
+      return new ToStringBuilder(Foo.class)
+        .append("id", id)
+        .append("related", related)
+        .toString();
     }
   }
 
