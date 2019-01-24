0e30957b3e3689b5d8d0f69ec8e8eca0bd5cb412
David Herges <dherges@users.noreply.github.com>
2019-01-24 13:16:47 +0100

    feat(components): add button options API

--- packages/components/button/src/button.options.ts
+++ packages/components/button/src/button.options.ts
@@ -3,7 +3,31 @@ import { Injectable } from '@angular/core';
 export type ButtonVariant = 'blue' | 'grey';
 
 @Injectable({ providedIn: 'root' })
-export class ButtonOptions {
+export interface ButtonOptions {
+  defaultVariant: ButtonVariant;
+  foo?: string;
+}
+
+export const BUTTON_DEFAULTS: ButtonOptions = {
+  defaultVariant: 'blue'
+};
+
+/** @internal */
+export function buttonOptionsFactory() {
+  const o = new ButtonOptionsDi();
+  Object.assign(o, BUTTON_DEFAULTS);
+  
+  return o;
+}
+
+/** @internal DI token for ButtonOptions interface */
+@Injectable({
+  providedIn: 'root',
+  useFactory: buttonOptionsFactory
+})
+export class ButtonOptionsDi implements ButtonOptions {
+  defaultVariant;
+}
 
   defaultVariant: ButtonVariant = 'blue';
 
