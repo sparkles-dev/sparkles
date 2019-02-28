4bfa29c67e7781908e3774478fe99b5a279ad034
David <david@spektrakel.de>
2019-02-28 22:16:03 +0100

    style: fix script

--- scripts/ci.frontend.sh
+++ scripts/ci.frontend.sh
@@ -4,7 +4,7 @@ set -e
 ###
 # On master, build all. On branches and pull requests, only build affected by change.
 # https://blog.nrwl.io/nrwl-nx-6-1-better-dev-ergonomics-faster-builds-3198bb310e39
-if [ "${CIRCLE_BRANCH}" = "master" ] || [ "${CIRCLE_TAG}" ]
+if [ "${CIRCLE_BRANCH}" = "master" ] || [ "${CIRCLE_TAG}" ]; then
   AFFECTED_ARGS = "--all"
 else
   AFFECTED_ARGS = "--base=origin/master --head=HEAD"
