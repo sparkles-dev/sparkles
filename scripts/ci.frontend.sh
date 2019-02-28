ea6458b08d5bd127f5cff1bf36d5fb52572dfb03
David <david@spektrakel.de>
2019-02-28 22:25:15 +0100

    style: fix script

--- scripts/ci.frontend.sh
+++ scripts/ci.frontend.sh
@@ -4,7 +4,7 @@ set -e
 ###
 # On master, build all. On branches and pull requests, only build affected by change.
 # https://blog.nrwl.io/nrwl-nx-6-1-better-dev-ergonomics-faster-builds-3198bb310e39
-if [ "${CIRCLE_BRANCH}" = "master" ] || [ "${CIRCLE_TAG}" ]; then
+if [ "${CIRCLE_BRANCH}" = "master" ] || [ -n "${CIRCLE_TAG}" ]; then
   AFFECTED_ARGS="--all"
 else
   AFFECTED_ARGS="--base=origin/master --head=HEAD"
@@ -12,5 +12,5 @@ fi
 
 yarn packages:styles:build
 yarn packages:components:build
-yarn affected:build "${AFFECTED_ARGS}"
-xvfb-run -a yarn affected:test "${AFFECTED_ARGS}"
+yarn affected:build ${AFFECTED_ARGS}
+xvfb-run -a yarn affected:test ${AFFECTED_ARGS}
