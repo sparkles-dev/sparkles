4dff05d935e9a05d8607298ce82d333c913b87ed
David <david@spektrakel.de>
2019-03-02 12:06:06 +0100

    build: verify commit messages for auto-generated changelogs

---
+++ commitlint.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  extends: ['@commitlint/config-angular'],
+  rules: {
+    'header-max-length': [
+      2,
+      'always',
+      120
+    ],
+    'scope-enum': [
+      2,
+      'always',
+      [
+        'domain',
+        'components',
+        'forms',
+        'reframed',
+        'shared',
+        'styles'
+      ]
+    ],
+    'scope-empty': [
+      1,
+      'never'
+    ],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'release',
+        'revert',
+        'style',
+        'test'
+      ]
+    ]
+  }
+};
