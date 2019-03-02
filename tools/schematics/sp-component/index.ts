8823a0827f9002535b7909564ce91461fdb5702a
David Herges <dherges@users.noreply.github.com>
2019-03-02 13:58:21 +0100

    build: add sp-component schematic to generate a component sub package (#261)

---
+++ tools/schematics/sp-component/index.ts
@@ -0,0 +1,45 @@
+import { apply, applyTemplates, chain, externalSchematic, mergeWith, move, url, Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { formatFiles } from '@nrwl/schematics/src/utils/rules/format-files';
+import { Schema } from './schema';
+
+export default function(schema: Schema): Rule {
+
+  return (host: Tree, context: SchematicContext) => {
+    if (!schema.name) {
+      context.logger.fatal('Component name must be set!', schema as any);
+      throw new Error(`Component name must be set!`);
+    }
+
+    const componentName = schema.name;
+    const componentPath = `packages/components/${componentName}`;
+    const templateSource = apply(url('./files'), [
+      applyTemplates(schema),
+      move(componentPath)
+    ]);
+
+    return chain([
+      externalSchematic('@schematics/angular', 'module', {
+        project: 'components',
+        path: `${componentPath}/src`,
+        flat: true,
+        commonModule: true
+      }),
+      externalSchematic('@schematics/angular', 'component', {
+        project: 'components',
+        path: `${componentPath}/src`,
+        flat: true,
+        changeDetection: 'OnPush',
+        export: true,
+        prefix: 'sp',
+        style: 'scss'
+      }),
+      mergeWith(templateSource),
+      formatFiles({
+        skipFormat: schema.skipFormat
+      }),
+      () => {
+        context.logger.info(`Component sp-${componentName} is generated in folder ${componentPath}.`);
+      }
+    ]);
+  };
+}
