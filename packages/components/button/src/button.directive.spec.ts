4af478d7c8ad45d281fec5b7a38a03789a04db86
David <david@spektrakel.de>
2019-03-14 21:35:17 +0100

    test: debug and correct jest tests

--- packages/components/button/src/button.directive.spec.ts
+++ packages/components/button/src/button.directive.spec.ts
@@ -1,20 +1,25 @@
 import { TestBed, async } from '@angular/core/testing';
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { DomElementRef, createTestComponent } from '@sparkles/testing';
 import { ButtonModule } from './button.module';
+import { ButtonDirective } from './button.directive';
 
 describe(`[spButton] ButtonDirective`, () => {
 
   @Component({
     template: `<button spButton>foo</button>`
   })
-  class TestComponent {}
+  class TestComponent {
+
+    @ViewChild(ButtonDirective)
+    public button: ButtonDirective;
+  }
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [ ButtonModule ],
       declarations: [ TestComponent ]
-    }).compileComponents();
+    });
   }));
 
   it('should create', () => {
@@ -37,10 +42,11 @@ describe(`[spButton] ButtonDirective`, () => {
   describe(`spButton`, () => {
 
     it(`should render .btn-blue`, () => {
-      const fixture = createTestComponent(TestComponent, `<button spButton="blue">foo</button>`);
+      const fixture = createTestComponent(TestComponent, `<button [spButton]="'blue'">foobar</button>`);
       fixture.detectChanges();
 
       expect(DomElementRef.create(fixture.elementRef).querySelector('.btn.btn-blue')).toBeTruthy();
+      fixture.destroy();
     });
 
     it(`should render .btn-grey`, () => {
@@ -48,6 +54,7 @@ describe(`[spButton] ButtonDirective`, () => {
       fixture.detectChanges();
 
       expect(DomElementRef.create(fixture.elementRef).querySelector('.btn.btn-grey')).toBeTruthy();
+      fixture.destroy();
     });
   });
 
