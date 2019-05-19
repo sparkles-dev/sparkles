50027fec33803ebb9a150c66ca09b4b98006e198
David <david@spektrakel.de>
2019-05-19 19:28:21 +0200

    style: fix deep import issues

--- libs/demos/app/src/lib/cdk/cdk-demo.module.ts
+++ libs/demos/app/src/lib/cdk/cdk-demo.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { CdkModule } from '@sparkles/components';
+// tslint:disable-next-line:nx-enforce-module-boundaries
 import { ButtonModule } from '@sparkles/components/button';
 import { DomOutletDemoComponent } from './dom-outlet-demo.component';
 import { HammerDemoComponent } from './hammer-demo.component';
