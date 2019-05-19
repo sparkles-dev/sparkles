1c35dd3954438bc69dc9fd74746dde93940ed230
David <david@spektrakel.de>
2019-05-19 22:23:15 +0200

    test: resolve test compile errors

--- apps/webcomponents/demo-element/src/app/app.component.spec.ts
+++ apps/webcomponents/demo-element/src/app/app.component.spec.ts
@@ -14,18 +14,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'webcomponents-demo-element'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('webcomponents-demo-element');
-  });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain(
-      'Welcome to webcomponents-demo-element!'
-    );
-  });
 });
