a9e7c65b189f760abb1412a72837478ddc583603
David <dherges@users.noreply.github.com>
2019-05-24 23:35:23 +0200

    test: resollve test failure

--- apps/rivers/rivers/src/app/app.component.spec.ts
+++ apps/rivers/rivers/src/app/app.component.spec.ts
@@ -1,9 +1,11 @@
 import { TestBed, async } from '@angular/core/testing';
+import { MapModule } from './map/map.module';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [MapModule],
       declarations: [AppComponent]
     }).compileComponents();
   }));
