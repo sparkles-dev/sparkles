147f84fa6b7a157a086e0a7fa37e46b660684ea5
David <dherges@users.noreply.github.com>
2019-08-11 14:20:58 +0200

    fix: extract opinionated base functionality to JavalinApp

--- apps/javalin/entity/src/main/java/sparkles/EntityApp.java
+++ apps/javalin/entity/src/main/java/sparkles/EntityApp.java
@@ -1,23 +1,14 @@
 package sparkles;
 
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.persistence.Persistence;
-import javax.sql.DataSource;
-
 import io.javalin.Javalin;
 
 import sparkles.entity.FooApi;
 
 import sparkles.support.common.Environment;
-import sparkles.support.javalin.flyway.FlywayExtension;
+import sparkles.support.javalin.JavalinApp;
 import sparkles.support.javalin.keycloak.security.KeycloakAccessManager;
 import sparkles.support.javalin.keycloak.security.KeycloakRoles;
 import sparkles.support.javalin.spring.data.auditing.AuditingExtension;
-import sparkles.support.javalin.spring.data.SpringDataExtension;
 
 public class EntityApp {
 
@@ -26,13 +17,8 @@ public class EntityApp {
   }
 
   public Javalin init() {
-    final DataSource dataSource = createDataSource();
 
-    return Javalin.create()
-      .register(FlywayExtension.create(() -> dataSource,
-        "persistence/migrations/flyway"))
-      .register(SpringDataExtension.create(() -> Persistence.createEntityManagerFactory(
-        "stuff", createHibernateProperties(dataSource))))
+    return JavalinApp.create("stuff")
       .register(AuditingExtension.create((ctx) -> {
         // TODO: resolve auditor from request context
         return "foo";
@@ -47,28 +33,6 @@ public class EntityApp {
       ))
       .register(new FooApi());
 
-  }
-
-  private static DataSource createDataSource() {
-    final DriverManagerDataSource ds = new DriverManagerDataSource();
-    ds.setDriverClassName(Environment.value("JDBC_DRIVER", "org.hsqldb.jdbc.JDBCDataSource"));
-    ds.setUrl(Environment.value("JDBC_URL", "jdbc:hsqldb:mem:standalone"));
-    ds.setUsername(Environment.value("JDBC_USER", "sa"));
-    ds.setPassword(Environment.value("JDBC_PASSWORD", ""));
-
-    return ds;
-  }
-
-  private static Map<String, Object> createHibernateProperties(DataSource dataSource) {
-    final Map<String, Object> props = new HashMap<String, Object>();
-    props.put("javax.persistence.nonJtaDataSource", dataSource);
-    props.put("javax.persistence.transactionType", "RESOURCE_LOCAL");
-    props.put("hibernate.show_sql", Environment.isDevelop());
-    props.put("hibernate.format_sql", false);
-    props.put("hibernate.hbm2ddl.auto", "validate");
-    props.put("hibernate.dialect", Environment.value("HIBERNATE_DIALECT", "org.hibernate.dialect.HSQLDialect"));
-
-    return props;
   }
 
 }
--- support/javalin/api/src/main/java/sparkles/support/javalin/JavalinApp.java
+++ support/javalin/api/src/main/java/sparkles/support/javalin/JavalinApp.java
@@ -3,26 +3,62 @@ package sparkles.support.javalin;
 import io.javalin.Javalin;
 import io.javalin.security.Role;
 import sparkles.support.common.Environment;
+import sparkles.support.common.collections.CollectionUtil;
+import sparkles.support.javalin.flyway.FlywayExtension;
+import sparkles.support.javalin.spring.data.SpringDataExtension;
 
 import java.util.Arrays;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-public class JavalinApp extends Javalin {
+import javax.persistence.Persistence;
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+public class JavalinApp {
   static {
     System.setProperty(org.slf4j.impl.SimpleLogger.LOG_KEY_PREFIX + "sparkles", Environment.logLevel());
   }
 
-  protected JavalinApp() {
-    super();
-  }
-
-  public static JavalinApp create() {
-    return new JavalinApp();
+  public static Javalin create() {
+    return Javalin.create();
   }
 
   public static Set<Role> requires(Role... roles) {
     return Arrays.stream(roles).collect(Collectors.toSet());
+  }
+
+  public static Javalin create(String appName) {
+    final DataSource dataSource = createDataSource();
+
+    return Javalin.create()
+      .register(FlywayExtension.create(() -> dataSource,
+        "persistence/migrations/flyway"))
+      .register(SpringDataExtension.create(() -> Persistence.createEntityManagerFactory(
+        appName, createHibernateProperties(dataSource))));
+  }
+
+  private static DataSource createDataSource() {
+    final DriverManagerDataSource ds = new DriverManagerDataSource();
+    ds.setDriverClassName(Environment.value("JDBC_DRIVER", "org.hsqldb.jdbc.JDBCDataSource"));
+    ds.setUrl(Environment.value("JDBC_URL", "jdbc:hsqldb:mem:standalone"));
+    ds.setUsername(Environment.value("JDBC_USER", "sa"));
+    ds.setPassword(Environment.value("JDBC_PASSWORD", ""));
+
+    return ds;
+  }
+
+  private static Map<String, Object> createHibernateProperties(DataSource dataSource) {
+    return CollectionUtil.<String, Object> newMap()
+      .put("javax.persistence.nonJtaDataSource", dataSource)
+      .put("javax.persistence.transactionType", "RESOURCE_LOCAL")
+      .put("hibernate.show_sql", Environment.isDevelop())
+      .put("hibernate.format_sql", false)
+      .put("hibernate.hbm2ddl.auto", "validate")
+      .put("hibernate.dialect", Environment.value("HIBERNATE_DIALECT", "org.hibernate.dialect.HSQLDialect"))
+      .build();
   }
 
 }
