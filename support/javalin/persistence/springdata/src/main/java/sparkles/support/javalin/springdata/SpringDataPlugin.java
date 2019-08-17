924439107474899be34906a85f780754c7f92be7
David Herges <dherges@users.noreply.github.com>
2019-08-17 09:02:42 +0200

    feat: begin database transactions only for request handlers that actually need one (#498)

--- support/javalin/persistence/springdata/src/main/java/sparkles/support/javalin/springdata/SpringDataPlugin.java
+++ support/javalin/persistence/springdata/src/main/java/sparkles/support/javalin/springdata/SpringDataPlugin.java
@@ -2,6 +2,7 @@ package sparkles.support.javalin.springdata;
 
 import io.javalin.core.plugin.Plugin;
 import io.javalin.http.Context;
+import io.javalin.http.Handler;
 import org.springframework.data.jpa.repository.support.JpaRepositoryFactory;
 
 import java.util.HashMap;
@@ -22,54 +23,72 @@ import lombok.extern.slf4j.Slf4j;
 public class SpringDataPlugin implements Plugin {
 
   private final Supplier<EntityManagerFactory> entityManagerFactory;
+  private final String paths;
 
   @Override
   public void apply(Javalin app) {
 
-    app
-      .events(evts -> {
-        evts.serverStarting(() -> {
-          log.debug("Creating EntityManagerFactory for JPA/Hibernate...");
-          app.attribute(EntityManagerFactory.class, entityManagerFactory.get());
-          log.info("Created EntityManagarFactory.");
-        });
-
-        evts.serverStopping(() -> {
-          log.debug("Closing EntityManagerFactory...");
-          app.attribute(EntityManagerFactory.class).close();
-          log.info("Closed EntityManagarFactory.");
-        });
-      })
-      .before(ctx -> {
-        // EntityManagerFactory is application scoped
-        EntityManagerFactory entityManagerFactory = ctx.appAttribute(EntityManagerFactory.class);
-        // EntityManager and JpaRepositoryFactory are request scoped
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        JpaRepositoryFactory jpaRepositoryFactory = new JpaRepositoryFactory(entityManager);
-
-        ctx.register(EntityManager.class, entityManager);
-        ctx.register(JpaRepositoryFactory.class, jpaRepositoryFactory);
-        ctx.register(SpringData.class, new SpringData(entityManager, jpaRepositoryFactory));
-
-        // Begin a transaction per request (transaction boundary is request boundary)
-        entityManager.getTransaction().begin();
-      })
-      .after(ctx -> {
-        EntityManager entityManager = springData(ctx).entityManager();
-
-        // Commit and close transaction
-        entityManager.getTransaction().commit();
-        entityManager.close();
+    // Register event listener for creation/closing of database connection
+    app.events(evts -> {
+      evts.serverStarting(() -> {
+        log.debug("Creating EntityManagerFactory for JPA/Hibernate...");
+        app.attribute(EntityManagerFactory.class, entityManagerFactory.get());
+        log.info("Created EntityManagarFactory.");
       });
 
+      evts.serverStopping(() -> {
+        log.debug("Closing EntityManagerFactory...");
+        app.attribute(EntityManagerFactory.class).close();
+        log.info("Closed EntityManagarFactory.");
+      });
+    });
+
+    // Register before and after handler
+    final Handler beforeHandler = ctx -> {
+      // EntityManagerFactory is application scoped
+      EntityManagerFactory entityManagerFactory = ctx.appAttribute(EntityManagerFactory.class);
+      // EntityManager and JpaRepositoryFactory are request scoped
+      EntityManager entityManager = entityManagerFactory.createEntityManager();
+      JpaRepositoryFactory jpaRepositoryFactory = new JpaRepositoryFactory(entityManager);
+
+      ctx.register(EntityManager.class, entityManager);
+      ctx.register(JpaRepositoryFactory.class, jpaRepositoryFactory);
+      ctx.register(SpringData.class, new SpringData(entityManager, jpaRepositoryFactory));
+
+      // Begin a transaction per request (transaction boundary is request boundary)
+      entityManager.getTransaction().begin();
+    };
+    final Handler afterHandler = ctx -> {
+      EntityManager entityManager = springData(ctx).entityManager();
+
+      // Commit and close transaction
+      entityManager.getTransaction().commit();
+      entityManager.close();
+    };
+    if (paths != null) {
+      app.before(paths, beforeHandler);
+      app.after(paths, afterHandler);
+    } else {
+      app.before(beforeHandler);
+      app.after(afterHandler);
+    }
+
+  }
+
+  public static SpringDataPlugin create(Supplier<EntityManagerFactory> entityManagerFactory, String paths) {
+    return new SpringDataPlugin(entityManagerFactory, paths);
   }
 
   public static SpringDataPlugin create(Supplier<EntityManagerFactory> entityManagerFactory) {
-    return new SpringDataPlugin(entityManagerFactory);
+    return new SpringDataPlugin(entityManagerFactory, null);
+  }
+
+  public static SpringDataPlugin create(String persistenceUnitName, Map<String, Object> hibernateProperties, String paths) {
+    return create(() -> Persistence.createEntityManagerFactory(persistenceUnitName, hibernateProperties));
   }
 
   public static SpringDataPlugin create(String persistenceUnitName, Map<String, Object> hibernateProperties) {
-    return create(() -> Persistence.createEntityManagerFactory(persistenceUnitName, hibernateProperties));
+    return create(persistenceUnitName, hibernateProperties, null);
   }
 
   public static SpringData springData(Context ctx) {
